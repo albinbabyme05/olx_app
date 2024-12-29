1321549a1537df06b699b6d223ae22c5a98bb2c4
Albin Baby <albinbabayme05@gmail.com>
2024-12-29 15:02:34 +0100

    with signup details user can login and logged in messgae will display if it is a successful login

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import './App.css'
 
 import {BrowserRouter as Router, Route} from  'react-router-dom'
 import Signup from './Pages/Signup'
+import Login from './Pages/Login'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,6 +18,9 @@ function App() {
         </Route>
         <Route  path='/signup'>
           <Signup />
+        </Route>
+        <Route  path='/login'>
+          <Login />
         </Route>
       </Router>
       
